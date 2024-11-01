1195c04312a056b86b56eff12a7e5011fb88d89f
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2024-11-01 11:59:40 -0400

    Restructure views for readability at scale

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -6,7 +6,7 @@
 <div class="overflow-hidden rounded-md bg-white shadow">
     <ul role="list" class="divide-y divide-gray-200">
 @foreach ($jobs as $job)
-    <a href="/job/{{$job['id']}}">
+    <a href="/jobs/{{$job['id']}}">
     <div>{{$job->employer->name}}</div>
     <li class="px-6 py-4">
         {{ $job['title']}}
--- routes/web.php
+++ routes/web.php
@@ -13,22 +13,22 @@ Route::get('/about', function () {
 });
 
 Route::get('/jobs', function () {
-    return view('jobs', [
-        'jobs' => Job::with('employer')->paginate(5),
+    return view('jobs.index', [
+        'jobs' => Job::with('employer')->simplePaginate(5),
         /* Other more performant pagination options for larger datasets:
             'jobs' => Job::with('employer')->cursorPaginate(5),
-            'jobs' => Job::with('employer')->simplePaginate(5),
+            'jobs' => Job::with('employer')->paginate(5),
             ** Amend display code with `php artisan vendor:publish` ***
         */
     ]);
 });
 
-Route::get('/job/{id}', function ($id) {
+Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
-    return view('job', ['job' => $job, 'tags' => $job->tags]);
+    return view('jobs.show', ['job' => $job, 'tags' => $job->tags]);
 });
 
-Route::get('/tag/{id}', function ($id) {
+Route::get('/tags/{id}', function ($id) {
     $tag = Tag::find($id);
-    return view('tag', ['tag' => $tag, 'jobs' => $tag->jobs]);
+    return view('tags.show', ['tag' => $tag, 'jobs' => $tag->jobs]);
 });
