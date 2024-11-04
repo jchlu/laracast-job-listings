61436997ef3eab8cc90d08e323886d4c09de95c8
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2024-11-04 15:39:33 -0400

    Refector to a route controller

---
+++ app/Http/Controllers/JobController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+use Illuminate\Http\Request;
+
+class JobController extends Controller
+{
+    public function index()
+    {
+        return view('jobs.index', ['jobs' => Job::with('employer')->latest()->paginate(5)]);
+    }
+
+    // Create job
+    public function create()
+    {
+        return view('jobs.create');
+    }
+
+    // Persist job
+
+    public function store()
+    {
+        request()->validate([
+            'title' => ['required', 'min:3'],
+            'salary' => ['required']
+        ]);
+
+        Job::create([
+            'title' => request('title'),
+            'salary' => request('salary'),
+            'employer_id' => fake()->numberBetween(1, 20)
+        ]);
+        return redirect('/jobs');
+    }
+
+    // Show job
+    public function show(Job $job)
+    {
+        return view('jobs.show', ['job' => $job]);
+    }
+
+    // Edit job
+    public function edit($id)
+    {
+        return view('jobs.edit', ['job' => Job::findOrFail($id)]);
+    }
+
+    //Update
+    public function update(Job $job)
+    {
+        //TODO: Auth
+        request()->validate([
+            'title' => ['required', 'min:3'],
+            'salary' => ['required']
+        ]);
+
+        // create a job from the request data AFTER VALIDATION
+        $job->update([
+            'title' => request('title'),
+            'salary' => request('salary'),
+        ]);
+        return redirect('/jobs/' . $job->id);
+    }
+
+    // Destroy job
+    public function destroy(Job $job)
+    {
+        // TODO:Auth
+        $job->delete();
+        return redirect('/jobs');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use App\Models\Job;
 use App\Models\Tag;
 use Illuminate\Support\Facades\Route;
@@ -17,55 +18,25 @@ Route::get('/about', fn() => view('about'));
 */
 
 // Create job
-Route::post('/jobs', function () {
-    // create a job from the request data AFTER VALIDATION
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]);
-
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => fake()->numberBetween(1, 20)
-    ]);
-    return redirect('/jobs');
-});
+Route::post('/jobs', [JobController::class, 'store']);
 
 // Update job
-Route::patch('/jobs/{id}', function () {
-    //TODO: Auth
-    // create a job from the request data AFTER VALIDATION
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]);
+Route::patch('/jobs/{job}', [JobController::class, 'update']);
 
-    Job::findOrFail(request('id'))->update([
-        'title' => request('title'),
-        'salary' => request('salary'),
-    ]);
-    return redirect('/jobs/' . request('id'));
-});
-
-// Show create
-Route::get('/jobs/create', fn() => view('jobs.create'));
+// Create job
+Route::get('/jobs/create', [JobController::class, 'create']);
 
 // Show job
-Route::get(('/jobs/{job}'), fn(Job $job) => view('jobs.show', ['job' => $job]));
+Route::get(('/jobs/{job}'), [JobController::class, 'show']);
 
 // Edit job
-Route::get('/jobs/{id}/edit', fn($id) => view('jobs.edit', ['job' => Job::findOrFail($id)]));
+Route::get('/jobs/{id}/edit', [JobController::class, 'edit']);
 
-// Delete job
-Route::delete('/jobs/{id}', function () {
-    // TODO:Auth
-    Job::findOrFail(request('id'))->delete();
-    return redirect('/jobs');
-});
+// Destroy job
+Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
 
 // Show all jobs
-Route::get('/jobs', fn() => view('jobs.index', ['jobs' => Job::with('employer')->latest()->paginate(5)]));
+Route::get('/jobs', [JobController::class, 'index']);
 
 // Show tag
 Route::get('/tags/{tag}', fn(Tag $tag) => view('tags.show', ['tag' => $tag]));
