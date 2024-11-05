612220fb1341a0cb17acae30293a1451a8a1f875
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2024-11-04 22:02:11 -0400

    Refactor routes to resources group

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,21 @@
 <?php
 
 use App\Http\Controllers\JobController;
-use App\Models\Job;
 use App\Models\Tag;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 Route::view('/about', ('about'));
 
-// Create job
-Route::post('/jobs', [JobController::class, 'store']);
-
-// Update job
-Route::patch('/jobs/{job}', [JobController::class, 'update']);
-
-// Create job
-Route::get('/jobs/create', [JobController::class, 'create']);
-
-// Show job
-Route::get(('/jobs/{job}'), [JobController::class, 'show']);
-
-// Edit job
-Route::get('/jobs/{id}/edit', [JobController::class, 'edit']);
-
-// Destroy job
-Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
-
-// Show all jobs
-Route::get('/jobs', [JobController::class, 'index']);
+Route::controller(JobController::class)->group(function () {
+  Route::post('/jobs', 'store');
+  Route::patch('/jobs/{job}', 'update');
+  Route::get('/jobs/create', 'create');
+  Route::get(('/jobs/{job}'), 'show');
+  Route::get('/jobs/{id}/edit', 'edit');
+  Route::delete('/jobs/{job}', 'destroy');
+  Route::get('/jobs', 'index');
+});
 
 // Show tag
 Route::get('/tags/{tag}', fn(Tag $tag) => view('tags.show', ['tag' => $tag]));
