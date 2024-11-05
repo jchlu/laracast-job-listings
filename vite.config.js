cf9f8b3260592de9fb1041327526e1ff4fa0d375
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2024-11-04 22:02:39 -0400

    Add local SSL for dev

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,28 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-
+import basicSsl from '@vitejs/plugin-basic-ssl'
+const host = '127.0.0.1';
+const port = '8000';
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+    basicSsl()
+  ],
+  server: {
+    // 005 enabling the HTTPS
+    https: true,
+    // 006 setting the proxy with Laravel as target (origin)
+    proxy: {
+      '^(?!(\/\@vite|\/resources|\/node_modules))': {
+        target: `http://${host}:${port}`,
+      }
+    },
+    host,
+    port: 5173,
+    // 007 be sure that you have the Hot Module Replacement
+    hmr: { host },
+  }
 });
