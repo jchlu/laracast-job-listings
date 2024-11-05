b4e1a13a7eeea6e050f8100c19c5c00e15ba49f5
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2024-11-04 22:27:25 -0400

    Refactor to use Laravel magic
    Auto routes resource for:
    index | create | show | edit | update | store | destroy

--- routes/web.php
+++ routes/web.php
@@ -7,15 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'home');
 Route::view('/about', ('about'));
 
-Route::controller(JobController::class)->group(function () {
-  Route::post('/jobs', 'store');
-  Route::patch('/jobs/{job}', 'update');
-  Route::get('/jobs/create', 'create');
-  Route::get(('/jobs/{job}'), 'show');
-  Route::get('/jobs/{id}/edit', 'edit');
-  Route::delete('/jobs/{job}', 'destroy');
-  Route::get('/jobs', 'index');
-});
+Route::resource('job', JobController::class);
 
 // Show tag
 Route::get('/tags/{tag}', fn(Tag $tag) => view('tags.show', ['tag' => $tag]));
